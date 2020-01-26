fcd887d139c181788ffe7278070709fd10c04f6c
mrql <linus.tlund@gmail.com>
2020-01-26 14:27:12 +0100

    Added base pages, and Razor pages support.

--- DT102G - Laboration 1.2/Startup.cs
+++ DT102G - Laboration 1.2/Startup.cs
@@ -16,6 +16,7 @@ namespace DT102G___Laboration_1._2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,7 @@ namespace DT102G___Laboration_1._2
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
             });
         }
     }
